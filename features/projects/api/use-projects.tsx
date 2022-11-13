fd849ea8492a0577323dc1325277d559183140d6
Nicolas Wendling <contact@nicolaswendling.com>
2022-11-13 18:04:44 +0100

    Wrap  wrong data from API

--- features/projects/api/use-projects.tsx
+++ features/projects/api/use-projects.tsx
@@ -4,7 +4,22 @@ import { Project } from "../types/project.types";
 
 async function getProjects() {
   const { data } = await axios.get("https://prolog-api.profy.dev/project");
-  return data;
+
+  const newData = [...data].map((project) => {
+    let newStatus = "warning";
+
+    if (project.status === "info") {
+      newStatus = "stable";
+    }
+
+    if (project.status === "error") {
+      newStatus = "critical";
+    }
+
+    return { ...project, status: newStatus };
+  });
+
+  return newData;
 }
 
 export function useProjects() {
