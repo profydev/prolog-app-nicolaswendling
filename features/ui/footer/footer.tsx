fc35734bea61ff32ce5f63c1fd87f0e21e9d4bf4
Nicolas Wendling <contact@nicolaswendling.com>
2022-11-14 12:39:13 +0100

    Implement Mobile CSS

--- features/ui/footer/footer.tsx
+++ features/ui/footer/footer.tsx
@@ -1,21 +1,27 @@
 import styled from "styled-components";
-import { color, textFont, space } from "@styles/theme";
+import { color, breakpoint, space } from "@styles/theme";
 import Image from "next/image";
 import Link from "next/link";
 
 const Container = styled.footer`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 60px;
-  padding: 0 ${space(8)};
   color: ${color("gray", 400)};
   background: ${color("gray", 50)};
+  text-align: center;
+  padding: ${space(8)};
+  @media (min-width: ${breakpoint("desktop")}) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 60px;
+    padding: 0 ${space(8)};
+  }
 `;
 
 const Nav = styled.nav`
   display: flex;
+  justify-content: center;
   gap: ${space(8)};
+  margin: auto auto ${space(8)};
 `;
 const Anchor = styled.a`
   color: ${color("gray", 500)};
@@ -23,14 +29,16 @@ const Anchor = styled.a`
   text-decoration: none;
 `;
 
+const Version = styled.span`
+  display: block;
+  margin: ${space(8)} auto auto;
+`;
+
 const version = "14.5.1";
 
 export function Footer() {
   return (
     <Container>
-      <span>
-        Version: <span>{version}</span>
-      </span>
       <Nav>
         <Link href="#" passHref>
           <Anchor>Docs</Anchor>
@@ -45,9 +53,10 @@ export function Footer() {
           <Anchor href="#">Community</Anchor>
         </Link>
       </Nav>
-      <span>
-        <Image src="/icons/logo-small.svg" alt="logo" width="23" height="33" />
-      </span>
+      <Image src="/icons/logo-small.svg" alt="logo" width="23" height="33" />
+      <Version>
+        Version: <span>{version}</span>
+      </Version>
     </Container>
   );
 }
